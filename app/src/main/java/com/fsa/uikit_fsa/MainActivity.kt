6b5ab2749408ce44e40c54361811680330ad476b
Firuza Aliyeva <firuza.aliyeva@procure.az>
2023-07-11 17:42:45 +0400

    library setup and test composable implemented

---
+++ app/src/main/java/com/fsa/uikit_fsa/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.fsa.uikit_fsa
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.fsa.mylibrary.composables.Kit_Button
+import com.fsa.uikit_fsa.ui.theme.UIKit_FSATheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UIKit_FSATheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Box(Modifier.fillMaxSize()) {
+
+                        Kit_Button(
+                            modifier = Modifier.align(Alignment.Center),
+                            "Meow",
+                            backgroundColor = Color.Red.copy(0.5f)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ mylibrary/src/main/java/com/fsa/mylibrary/composables/Button.kt
@@ -0,0 +1,39 @@
+package com.fsa.mylibrary.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Kit_Button(
+    modifier: Modifier = Modifier,
+    btnText: String = "",
+    backgroundColor: Color = Color.White,
+    onClick: () -> Unit = {}
+) {
+    Box(
+        modifier = modifier
+            .clickable { onClick() }
+            .wrapContentSize()
+            .background(backgroundColor),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(btnText, modifier = Modifier.padding(12.dp))
+    }
+}
+
+@Preview
+@Composable
+fun Preview() {
+    Kit_Button(btnText = "Meow!", modifier = Modifier.width(100.dp))
+}
