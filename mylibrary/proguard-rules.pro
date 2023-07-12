eed961d123d1a98d2c958fefd208f2fdfdb05f46
Firuza Aliyeva <firuza.aliyeva@procure.az>
2023-07-12 14:53:32 +0400

    obfuscation keep via annotation

--- mylibrary/proguard-rules.pro
+++ mylibrary/proguard-rules.pro
@@ -19,7 +19,3 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
-
--keep class !com.fsa.mylibrary.core.hidden.** { *; }
--keep class org.lwjgl.** { *; }
--keep class com.badlogic.** { *; }
--- mylibrary/src/main/java/com/fsa/mylibrary/core/Api.kt
+++ mylibrary/src/main/java/com/fsa/mylibrary/core/Api.kt
@@ -2,6 +2,7 @@ package com.fsa.mylibrary.core
 
 import android.content.Context
 import android.widget.Toast
+import androidx.annotation.Keep
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -9,6 +10,7 @@ import com.fsa.mylibrary.core.hidden.Kit_Button
 import com.fsa.mylibrary.core.hidden.calculateSomeMeaninglessStuff
 
 
+@Keep
 @Composable
 fun Kit_Button_Functional(
     ctx: Context,
@@ -25,6 +27,7 @@ fun Kit_Button_Functional(
     })
 }
 
+@Keep
 @Composable
 fun Kit_Button_Functional2(
     ctx: Context,
@@ -41,6 +44,7 @@ fun Kit_Button_Functional2(
     })
 }
 
+@Keep
 fun generateString(a: Int, b: Int): String {
     return calculateSomeMeaninglessStuff(a, b)
 }
